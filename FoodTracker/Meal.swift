7e8575e2d2b00f93f84b2a5ad3cea1370dc44fe3
aclopesjr <aclopesjr@hotmail.com>
2016-06-17 21:01:44 -0300

    Implementa the "Define Your Data Model" chapter from https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson6.html#//apple_ref/doc/uid/TP40015214-CH20-SW1

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Antonio Lopes Jr on 17/06/2016.
+//  Copyright © 2016 EasyMob. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
